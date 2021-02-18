7552bf4ca34b701a40e05398d35718fb7305536c
Sean Knowles <s@seanknowles.dev>
2021-02-18 15:57:22 +0000

    homework submission state

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import './css/main.scss'
 
-console.log('index.js connected and working')
+const state = {
+  stagingList: [''],
+  teamsCount: 0
+}
+
+const stagingComponent = (stagingList = []) => {
+  if (stagingList.length === 0 && stagingList === undefined) {
+    console.log('no members')
+  }
+  const staging = document.getElementById('staging-list')
+  const member = document.createElement('li')
+  member.classList = 'list-group-item'
+  member.innerText = `${stagingList[0]}`
+  staging.appendChild(member)
+}
+
+// button handles members to list
+document.getElementById('btnAdd').addEventListener('click', (event) => {
+  const { value } = document.getElementById('inputMembers')
+  state.stagingList.unshift(value)
+  stagingComponent(state.stagingList)
+})
+
+// TODO button doesn't work
+// const addButton = () => {
+//   console.log('working')
+// }
+
+window.onload = function () {
+  stagingComponent()
+}
